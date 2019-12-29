9bf0d42a365184bd15e5b974cf8a593a8cec5e99
tusharkawsar <tushar.kawsar@gmail.com>
2019-12-29 07:57:50 -0500

    reg exp

--- regexpr.py
+++ regexpr.py
@@ -1,9 +1,45 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Dec 28 11:41:55 2019
-
-@author: tushar
+\w \d \s . \.
+[aAbB]
+[a-z] [A-Z] [0-9] [A-Za-z]
+a* a+ a? a{2,4}
 """
 
-s
+import re
+txt = "The rain in Spain"
+x = re.search("^The.*Spain$", txt)
+if x:
+    print("yes!")
+else:
+    print("no")
+pattern = re.compile(r'[\w\.]+@[a-z]+\.[a-z]+')
+text = 'john.smith@mailbox.com is the e-mail of '\
+'John. He often writes to his boss at '\
+'boss@company.com. But the messages get forwarded '\
+'to his secretary at info@company.com.'
+x = re.finditer(pattern, text)
+print(x)
+for match in x:
+    print(match)
+    print(match.group())
+    print(match.start())
+    print(match.end())
+
+substring = re.findall(pattern, text)
+print(substring)
+split_list = re.split(pattern, text)
+print(split_list)
+
+
+
+
+
+
+
+
+
+
+
+
